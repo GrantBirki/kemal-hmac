3314d8aa2a16957a3d86121ca53ed882eea3f6ad
GrantBirki <grant.birkinbine@gmail.com>
2024-10-14 17:01:56 -0700

    add logic for loading client hmac secrets

--- src/kemal-hmac/handler.cr
+++ src/kemal-hmac/handler.cr
@@ -16,48 +16,118 @@ module Kemal::Hmac
     HMAC_TIMESTAMP_HEADER        = ENV.fetch("HMAC_TIMESTAMP_HEADER", "HTTP_X_HMAC_TIMESTAMP")
     HMAC_TOKEN_HEADER            = ENV.fetch("HMAC_TOKEN_HEADER", "HTTP_X_HMAC_TOKEN")
     HMAC_TIMESTAMP_SECOND_WINDOW = ENV.fetch("HMAC_TIMESTAMP_SECOND_WINDOW", 30).to_i
+    HMAC_REJECTED_CODE           = ENV.fetch("HMAC_REJECTED_CODE", 401).to_i
     HMAC_REJECTED_MESSAGE_PREFIX = ENV.fetch("HMAC_REJECTED_MESSAGE_PREFIX", "Unauthorized:")
+    HMAC_KEY_SUFFIX_LIST         = ENV.fetch("HMAC_KEY_SUFFIX_LIST", "HMAC_SECRET_BLUE,HMAC_SECRET_GREEN").split(",").map(&.strip)
+    HMAC_KEY_DELIMITER           = ENV.fetch("HMAC_KEY_DELIMITER", "_")
 
+    # initialize the Kemal::Hmac::Handler
+    # note: "BLUE" and "GREEN" in this context are two different secrets for the same client. This is a common pattern to allow for key rotation without downtime.
+    # examples:
+    #  hmac_client_header: "HTTP_X_HMAC_CLIENT"
+    #  hmac_timestamp_header: "HTTP_X_HMAC_TIMESTAMP"
+    #  hmac_token_header: "HTTP_X_HMAC_TOKEN"
+    #  timestamp_second_window: 30
+    #  rejected_code: 401 
+    #  rejected_message_prefix: "Unauthorized:"
+    #  hmac_key_suffix_list: ["HMAC_SECRET_BLUE", "HMAC_SECRET_GREEN"] - only used for env variable lookups
+    #  hmac_key_delimiter: "_" - only used for env variable lookups
+    #  hmac_secrets: {"cool-client-service" => ["BLUE_SECRET", "GREEN_SECRET"]} - explicitly set secrets for one or many clients
     def initialize(
       hmac_client_header : String? = nil,
       hmac_timestamp_header : String? = nil,
       hmac_token_header : String? = nil,
       timestamp_second_window : Int32? = nil,
-      rejected_message_prefix : String? = nil
+      rejected_code : Int32? = nil,
+      rejected_message_prefix : String? = nil,
+      hmac_key_suffix_list : Array(String)? = nil,
+      hmac_key_delimiter : String? = nil,
+      hmac_secrets : Hash(String, Array(String)) = {} of String => Array(String),
     )
       @hmac_client_header = hmac_client_header || HMAC_CLIENT_HEADER
       @hmac_timestamp_header = hmac_timestamp_header || HMAC_TIMESTAMP_HEADER
       @hmac_token_header = hmac_token_header || HMAC_TOKEN_HEADER
       @timestamp_second_window = timestamp_second_window || HMAC_TIMESTAMP_SECOND_WINDOW
+      @rejected_code = rejected_code || HMAC_REJECTED_CODE
       @rejected_message_prefix = rejected_message_prefix || HMAC_REJECTED_MESSAGE_PREFIX
+      @hmac_key_suffix_list = hmac_key_suffix_list || HMAC_KEY_SUFFIX_LIST
+      @hmac_key_delimiter = hmac_key_delimiter || HMAC_KEY_DELIMITER
+
       @required_hmac_headers = [
         @hmac_client_header,
         @hmac_timestamp_header,
         @hmac_token_header,
       ]
+
+      @secrets_cache = hmac_secrets
     end
 
     def call(context)
       headers = load_hmac_headers(context)
-      missing_headers = missing_hmac_headers(headers)
+      missing_headers, empty_headers = validate_hmac_headers(headers)
 
-      # if any of the required headers are missing, return 401
+      # if any of the required headers are missing, reject the request
       unless missing_headers.empty?
-        context.response.status_code = 401
+        context.response.status_code = @rejected_code
         context.response.headers["missing-hmac-headers"] = missing_headers.join(",")
         context.response.print "#{@rejected_message_prefix} missing required hmac headers"
         return
       end
 
+      # if any of the required headers are empty, reject the request
+      unless empty_headers.empty?
+        context.response.status_code = @rejected_code
+        context.response.headers["empty-hmac-headers"] = empty_headers.join(",")
+        context.response.print "#{@rejected_message_prefix} empty required hmac headers"
+        return
+      end 
+
       timestamp_result = recent_timestamp?(headers[@hmac_timestamp_header].not_nil!, @timestamp_second_window)
 
       unless timestamp_result[:valid]
-        context.response.status_code = 401
+        context.response.status_code = @rejected_code
         context.response.print "#{@rejected_message_prefix} #{timestamp_result[:message]}"
         return
       end
 
+      # attempt to load the secrets for the given client
+      client_secrets = load_secrets(headers[@hmac_client_header].not_nil!)
+
+      # reject the request if no secrets are found for the given client
+      unless client_secrets.any?
+        context.response.status_code = @rejected_code
+        context.response.print "#{@rejected_message_prefix} no secrets found for client: #{headers[@hmac_client_header].not_nil!}"
+        return
+      end 
+
       context.kemal_authorized_client = headers[@hmac_client_header]
+    end
+
+    # Load the secrets for the given client
+    # Returns an array of strings (secrets) with all posible secrets for the given client (BLUE + GREEN) 
+    def load_secrets(client : String) : Array(String)
+      key = client.upcase
+      unless KEY_VALIDATION_REGEX.match(key)
+        raise ArgumentError.new("Client name must only contain letters, numbers, or _")
+      end
+
+      # before doing a full lookup, check the cache first
+      if @secrets_cache.has_key?(key)
+        return @secrets_cache[key]
+      end
+
+      # if we make it here, check the environment variables
+      client_secrets = @hmac_key_suffix_list.map do |suffix|
+        env_key = [key, suffix].join(@hmac_key_delimiter).upcase
+        ENV.fetch(env_key, nil)
+      end.compact
+
+      # if client_secrets is not empty, add it to the cache
+      if !client_secrets.empty?
+        @secrets_cache[key] = client_secrets
+      end
+
+      return client_secrets
     end
 
     # Check if the timestamp is within the last `seconds` seconds and not in the future
@@ -90,9 +160,12 @@ module Kemal::Hmac
       end
     end
 
-    # If any of the required headers are missing, return the missing headers
-    def missing_hmac_headers(headers : Hash(String, String?)) : Array(String)
-      headers.select { |_, v| v.nil? }.keys
+    # If any of the required headers are missing or empty, return the missing or empty headers
+    def validate_hmac_headers(headers : Hash(String, String?)) : Array(Array(String))
+      missing_headers = headers.select { |_, v| v.nil? }.keys
+      empty_headers = headers.select { |_, v| v.try(&.empty?) }.keys
+    
+      return [missing_headers, empty_headers]
     end
   end
 end
