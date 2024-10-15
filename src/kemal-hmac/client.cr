3c0557a350c38a39ebeccb97dd9b1907ddfdba76
GrantBirki <grant.birkinbine@gmail.com>
2024-10-14 23:09:07 -0700

    add comments

--- src/kemal-hmac/client.cr
+++ src/kemal-hmac/client.cr
@@ -2,6 +2,9 @@ require "./token"
 
 module Kemal::Hmac
   class Client
+    # :param client: the client name which will be sending HTTP requests to the server using HMAC auth (String)
+    # :param secret: the secret used to generate the HMAC token in relation to the client (String)
+    # # :param algorithm: the algorithm used to generate the HMAC token (String) - defaults to SHA256
     def initialize(client : String, secret : String, algorithm : String? = "SHA256")
       @client = client
       @secret = secret
@@ -13,7 +16,11 @@ module Kemal::Hmac
       end
     end
 
-    def generate_headers(path : String)
+    # A public helper method to generate the HMAC headers for a given path
+    # Use this method to get pre-filled headers to send with your request to the server
+    # :param path: the path (HTTP path) to generate the headers for (String) - e.g. "/api/path"
+    # :return: a Hash of the HMAC headers
+    def generate_headers(path : String) : Hash(String, String)
       timestamp = Time::Format::ISO_8601_DATE_TIME.format(Time.utc)
       hmac_token = Kemal::Hmac::Token.new(@client, path, timestamp).hexdigest(@secret)
 
