70b9e851faab3015ba5a5af8b4c47912c018b2c7
GrantBirki <grant.birkinbine@gmail.com>
2024-10-16 14:14:16 -0700

    acceptance tests for qsp

--- script/acceptance_tests/tests.cr
+++ script/acceptance_tests/tests.cr
@@ -18,10 +18,24 @@ describe "crest" do
     response.body.should contain "Hi, my_crest_client!"
   end
 
+  it "successfully sends a request with HMAC auth with the crest client to the crest-client endpoint with query string params" do
+    client = Kemal::Hmac::Client.new("my_crest_client", "my_secret")
+
+    path = "/crest-client"
+
+    response = Crest.get(
+      "http://localhost:3000#{path}",
+      headers: client.generate_headers(path)
+    )
+
+    response.status_code.should eq 200
+    response.body.should contain "Hi, my_crest_client!"
+  end
+
   it "fails to send a request with HMAC auth with the crest client to the crest-client endpoint" do
     client = Kemal::Hmac::Client.new("my_crest_client", "incorrect_secret")
 
-    path = "/crest-client"
+    path = "/crest-client?foo=bar&q=baz&operation=1/2"
 
     begin
       Crest.get(
@@ -40,6 +54,22 @@ describe "http/client" do
     client = Kemal::Hmac::Client.new("my_standard_client", "my_secret_2")
 
     path = "/http-client"
+
+    headers = HTTP::Headers.new
+    client.generate_headers(path).each do |key, value|
+      headers.add(key, value)
+    end
+
+    response = HTTP::Client.get("http://localhost:3000#{path}", headers: headers)
+
+    response.status_code.should eq 200
+    response.body.should contain "Hi, my_standard_client!"
+  end
+
+  it "successfully sends a request with HMAC auth with the http-client to the http-client endpoint with query string params" do
+    client = Kemal::Hmac::Client.new("my_standard_client", "my_secret_2")
+
+    path = "/http-client?foo=bar&q=baz&operation=1/2"
 
     headers = HTTP::Headers.new
     client.generate_headers(path).each do |key, value|
